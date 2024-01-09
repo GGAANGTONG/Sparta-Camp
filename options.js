b5368a0f3477f151e92bec05dbbb24aefde717d2
GGANGTONG <qkrds0914@gmail.com>
2024-01-09 18:06:03 +0900

    최종

---
+++ options.js
@@ -0,0 +1,8 @@
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZTkxOWU0YzFkZGYzOTdjMjIzMjNkNzJiNjRkYjZjZCIsInN1YiI6IjY1OThkMGMwMWQxYmY0MDE0ZDIyZDFkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.13Uig7dMzOVBHUoAiuiUr2YR-TK3c5pjMPcK98AOs1Y",
+  },
+};
